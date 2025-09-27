fe7920cf646db8856f3e8ad2cd868a60a681db79
liza-rawr <alisah121173@gmail.com>
2025-09-27 14:40:42 +0700

    first commit

---
+++ tugas koding dan ai/datapribadi.py
@@ -0,0 +1,7 @@
+# File: datapribadi.py
+# Progam ini menampilkan informasi dasar siswa.
+
+
+print("Nama Lengkap : Kaliza Raflani")
+print("Kelas : X RPL1")
+print("Motivasi : Error itu bukan kegagalan, tapi petunjuk dari sistem semesta agar kita jadi lebih tangguh")
